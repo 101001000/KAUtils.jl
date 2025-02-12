3db39aecac6b759ad44b35d376577218c636bd59
101001000 <kike.delacalle.montilla@gmail.com>
2025-02-12 15:32:55 +0100

    KA namespace fixes

--- src/Arrays.jl
+++ src/Arrays.jl
@@ -1,17 +1,17 @@
 function ArrayConstructor(backend, arr)
-    d_arr = KernelAbstractions.allocate(backend, eltype(arr), size(arr))
-    KernelAbstractions.copyto!(backend, d_arr, arr)
-    KernelAbstractions.synchronize(backend)
+    d_arr = KA.allocate(backend, eltype(arr), size(arr))
+    KA.copyto!(backend, d_arr, arr)
+    KA.synchronize(backend)
     return d_arr
 end
 
 function ArrayConstructor(backend, T::Type, ::UndefInitializer, dims...)
-    d_arr = KernelAbstractions.allocate(backend, T, dims...)
+    d_arr = KA.allocate(backend, T, dims...)
     return d_arr
 end
 
-zeros(backend, T::Type, dims...) = KernelAbstractions.zeros(backend, T, dims...)
-zeros(backend, dims...) = KernelAbstractions.zeros(backend, Float32, dims...)
-ones(backend, T::Type, dims...) = KernelAbstractions.ones(backend, T, dims...)
-ones(backend, dims...) = KernelAbstractions.ones(backend, Float32, dims...)
+zeros(backend, T::Type, dims...) = KA.zeros(backend, T, dims...)
+zeros(backend, dims...) = KA.zeros(backend, Float32, dims...)
+ones(backend, T::Type, dims...) = KA.ones(backend, T, dims...)
+ones(backend, dims...) = KA.ones(backend, Float32, dims...)
 
